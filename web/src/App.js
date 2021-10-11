19f56618c0461960fcd6d26ae486cd26249ea88f
PiaoZhengxun <pjh951218@naver.com>
2021-10-11 21:02:27 +0900

    Revert "socialLogin"
    This reverts commit a2c724983e1fdc83a7e1519a83a04b41e9c10e40.

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
-import kakaoimg from "../src/kakao.png";
-import { KAKAO_URL } from "./socialLoginAuth";
-import axios from "axios";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <a href="http://3.38.97.234:8000/core-service/oauth2/authorization/kakao">
-        <img src={kakaoimg} width="222" />
-      </a>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
