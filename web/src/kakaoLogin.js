a2c724983e1fdc83a7e1519a83a04b41e9c10e40
PiaoZhengxun <pjh951218@naver.com>
2021-10-11 20:55:08 +0900

    socialLogin

---
+++ web/src/kakaoLogin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+const { Kakao } = window;
+
+const KakaoLogin = () => {
+  <div>wfwf</div>;
+};
+
+export default KakaoLogin;
---
+++ web/src/socialLoginAuth.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = "e44c5d0df42c6613c266e56204d9457b";
+const REDIRECT_URI = "http://localhost:3000/login/oauth2/code/kakao";
+
+// export const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_URL =
+  "http://3.38.97.234:8000/core-service/oauth2/authorization/kakao";
