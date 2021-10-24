1659f90ecb8d696067f90db84161d43e9172a253
PiaoZhengxun <pjh951218@naver.com>
2021-10-25 01:05:55 +0900

    Login

--- web/src/Pages/LoginPage/LoginPage.js
+++ web/src/Pages/LoginPage/LoginPage.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import { userLogin } from "api";
+import { kakaoImage } from "../../src/Images/kakaoLogin.PNG";
+import { naverImage } from "../../src/Images/naverLogin.PNG";
+var token;
 
 function LoginPage() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    userLogin
+      .userLoginApi(email, password)
+      .then((response) => {
+        token = response.headers.authorization;
+        localStorage.setItem("authorization", token);
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        alert("로그인 실패", error);
+        console.log("LoginPage.js <- 여기서 login 부분 실패");
+      });
+  };
+
   return (
     <div>
       <div>
@@ -13,6 +43,7 @@ function LoginPage() {
               name="email"
               id="email"
               placeholder="email@email.com"
+              onChange={onEmailChange}
             />
           </FormGroup>
           <FormGroup>
@@ -22,17 +53,20 @@ function LoginPage() {
               name="password"
               id="password"
               placeholder="PASSWORD"
+              onChange={onPasswordChange}
             />
           </FormGroup>
-          <Button>Login</Button>
         </Form>
       </div>
       <div>
+        <button onClick={login}>Login</button>
+      </div>
+      <div>
         <a href="http://3.38.97.234:8000/core-service/oauth2/authorization/kakao">
-          <img src="../../src/Images/kakaoLogin.PNG" alt="카카오" width="55" />
+          <img src={kakaoImage} alt="카카오" width="55" />
         </a>
         <a href="http://3.38.97.234:8000/core-service/oauth2/authorization/naver">
-          <img src="../../src/Images/naverLogin.PNG" alt="네이버" width="55" />
+          <img src={naverImage} alt="네이버" width="55" />
         </a>
       </div>
     </div>
