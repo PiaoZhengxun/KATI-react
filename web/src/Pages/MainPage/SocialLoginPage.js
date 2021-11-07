81673ab6edf249f32b4f3cd5b8bcaedd10dd8d6e
PiaoZhengxun <pjh951218@naver.com>
2021-11-07 17:16:47 +0900

    Update SocialLoginPage.js

--- web/src/Pages/MainPage/SocialLoginPage.js
+++ web/src/Pages/MainPage/SocialLoginPage.js
@@ -10,26 +10,15 @@ function SocialLoginPage() {
     setCode(socialCode);
     console.log("여기서 카카오 토큰 발행");
     axios
-      .post(
-        "http://kauth.kakao.com/oauth/token",
-        {
-          grant_type: "authorization_code",
+      .post("https://kauth.kakao.com/oauth/token", {
+        grant_type: "authorization_code",
+        client_id: "e44c5d0df42c6613c266e56204d9457b",
+        redirect_uri: "http://localhost:3000/login/ouath2/code/kakao",
+        code: code,
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded;character=utf-8",
         },
-        {
-          client_id: "e44c5d0df42c6613c266e56204d9457b",
-        },
-        {
-          code: code,
-        },
-        {
-          redirect_uri: "http://localhost:3000/login/ouath2/code/kakao",
-        },
-        {
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded;character=utf-8",
-          },
-        }
-      )
+      })
       .then((response) => {
         console.log(response, "useEffect Response 출력 부분");
       })
