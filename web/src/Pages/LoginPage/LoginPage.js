2f564527e958d615981c77b1fdd936e8f3615da2
PiaoZhengxun <pjh951218@naver.com>
2021-11-08 01:18:21 +0900

    socialLoginSuccess

--- web/src/Pages/LoginPage/LoginPage.js
+++ web/src/Pages/LoginPage/LoginPage.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Button, Form, FormGroup, Label, Input, Col,Row } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, Col, Row } from "reactstrap";
 import { getKakaoLogin, userLogin } from "../../api";
-import './LoginPageStyle.scss';
-import {SiNaver,SiKakaotalk} from 'react-icons/si';
+import "./LoginPageStyle.scss";
+import { SiNaver, SiKakaotalk } from "react-icons/si";
 import queryString from "query-string";
 import { KAKAO_AUTH_URL } from "./KakaoAuth";
 
@@ -50,81 +50,96 @@ function LoginPage() {
   };
 
   return (
-    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ fontSize: "3rem", marginBottom: "2rem" }}>KATI</div>
 
-    <div style={{fontSize:'3rem',marginBottom:"2rem"}}>KATI</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Form>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="exampleEmail">이메일</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  placeholder="email@email.com"
+                  type="email"
+                  onChange={onEmailChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="examplePassword">비밀번호</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="PASSWORD"
+                  type="password"
+                  onChange={onPasswordChange}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
 
-    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
-    <Form>
-<Row form>
-  <Col md={6}>
-    <FormGroup>
-      <Label for="exampleEmail">
-        이메일
-      </Label>
-      <Input
-        id="exampleEmail"
-        name="email"
-        placeholder="email@email.com"
-        type="email"
-        onChange={onEmailChange}
-      />
-    </FormGroup>
-  </Col>
-  <Col md={6}>
-    <FormGroup>
-      <Label for="examplePassword">
-        비밀번호
-      </Label>
-      <Input
-        id="examplePassword"
-        name="password"
-        placeholder="PASSWORD"
-        type="password"
-        onChange={onPasswordChange}
-      />
-    </FormGroup>
-  </Col>
-</Row>
+          <FormGroup check>
+            <Input id="exampleCheck" name="check" type="checkbox" />
+            <Label check for="exampleCheck" style={{ fontSize: "small" }}>
+              이메일 기억하기
+            </Label>
+          </FormGroup>
+          <button
+            className="login_btn"
+            style={{ width: "100%", height: "50px" }}
+          >
+            로그인
+          </button>
+          <div
+            style={{ display: "flex", marginTop: "10px", marginBottom: "50px" }}
+          >
+            <div className="select_btn">회원가입</div>
+            <div className="select_btn">이메일 찾기</div>
+            <div className="select_btn">비밀번호 찾기</div>
+          </div>
+          <Button
+            className="social_login_btn"
+            style={{ backgroundColor: "white", border: "1px solid #B1B1B1" }}
+            href={KAKAO_AUTH_URL}
+          >
+            {/* //http://3.38.97.234:8000/core-service/oauth2/authorization/kakao */}
+            <div>
+              <SiKakaotalk size="30" color="#3a1c1c" />
+            </div>
+            <div>카카오 로그인</div>
+          </Button>
 
-<FormGroup check>
-  <Input
-    id="exampleCheck"
-    name="check"
-    type="checkbox"
-  />
-  <Label
-    check
-    for="exampleCheck"
-    style={{fontSize:"small"}}
-  >
-    이메일 기억하기
-  </Label>
-</FormGroup>
-<button className="login_btn" style={{width:'100%',height:'50px'}}>
-  로그인
-</button>
-<div  style={{display:'flex', marginTop:'10px',marginBottom:'50px'}}>
-    <div className="select_btn">회원가입</div>
-    <div className="select_btn">이메일 찾기</div>
-    <div className="select_btn">비밀번호 찾기</div>
-</div>
-<Button className="social_login_btn" style={{backgroundColor:'white',border:'1px solid #B1B1B1'}}
-href="http://3.38.97.234:8000/core-service/oauth2/authorization/kakao">
-       <div><SiKakaotalk size="30" color="#3a1c1c"/></div>
-       <div>카카오 로그인</div>
-        </Button>
-
-<Button className="social_login_btn" style={{backgroundColor:'white',border:'1px solid #B1B1B1'}}
-href="http://3.38.97.234:8000/core-service/oauth2/authorization/naver">
-       <div><SiNaver size="30" color="#2DB400"/></div>
-       <div>네이버 로그인</div>
-        </Button>
-
-</Form>
-  </div>
-
-  </div>
+          <Button
+            className="social_login_btn"
+            style={{ backgroundColor: "white", border: "1px solid #B1B1B1" }}
+            href="http://3.38.97.234:8000/core-service/oauth2/authorization/naver"
+          >
+            <div>
+              <SiNaver size="30" color="#2DB400" />
+            </div>
+            <div>네이버 로그인</div>
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
 
--- web/src/Pages/MainPage/SocialLoginPage.js
+++ web/src/Pages/MainPage/SocialLoginPage.js
@@ -2,50 +2,80 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function SocialLoginPage() {
-  const [code, setCode] = useState("");
+  // const [code, setCode] = useState("");
+  const REDIRECT_URI = "http://localhost:3000/login/ouath2/code/kakao";
+  const REST_API_KEY = "e44c5d0df42c6613c266e56204d9457b";
+  const CLIENT_SECRET = "Rf1yNHjgzlieBWNm7T4ZOvSaASvNZ8UV";
+  const [bearer, setBearer] = useState("");
 
   useEffect(() => {
-    console.log("소셜로그인 useEffect");
-    let socialCode = new URL(window.location.href).searchParams.get("code");
-    setCode(socialCode);
-    console.log("여기서 카카오 토큰 발행");
-    axios
-      .post("https://kauth.kakao.com/oauth/token", {
-        grant_type: "authorization_code",
-        client_id: "e44c5d0df42c6613c266e56204d9457b",
-        redirect_uri: "http://localhost:3000/login/ouath2/code/kakao",
-        code: code,
-        headers: {
-          "Content-type": "application/x-www-form-urlencoded;character=utf-8",
-        },
-      })
-      .then((response) => {
-        console.log(response, "useEffect Response 출력 부분");
-      })
-      .then((error) => {
-        console.log(error, "useEffect 에러 부분");
-      });
-    // axios({
-    //   method: "POST",
-    //   url: "kauth.kakao.com/oauth/token",
-    //   {
-
-    //     code: code,
-
-    //   },
-    //   headers: {
-    //     "Content-type": "application/x-www-form-urlencoded;character=utf-8",
-    //   },
-    // }).then((response) => {
-    //   let token = `response.token_type`;
-    //   console.log(token);
-    // });
+    const code = window.location.search.split("?code=")[1];
+    if (code !== undefined) {
+      requestToken(code)
+        .then(({ data }) => {
+          console.log("requestToken:", data);
+          setBearer(data);
+          console.log(bearer, "토큰 부분");
+        })
+        .catch((err) => {
+          console.log("requestTokenErr:", err);
+        });
+    }
   }, []);
+
+  function requestToken(code) {
+    const makeFormData = (params) => {
+      const searchParams = new URLSearchParams();
+      Object.keys(params).forEach((key) => {
+        searchParams.append(key, params[key]);
+      });
+      return searchParams;
+    };
+    return axios({
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+      url: "https://kauth.kakao.com/oauth/token",
+      data: makeFormData({
+        grant_type: "authorization_code",
+        client_id: REST_API_KEY,
+        redirect_uri: REDIRECT_URI,
+        code,
+      }),
+    });
+  }
+
+  // useEffect(() => {
+  //   console.log("소셜로그인 useEffect");
+  //   let socialCode = new URL(window.location.href).searchParams.get("code");
+  //   setCode(socialCode);
+  //   console.log("여기서 카카오 토큰 발행");
+  //   axios
+  //     .post("https://kauth.kakao.com/oauth/token", {
+  //       headers: {
+  //         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+  //       },
+  //       data: {
+  //         grant_type: "authorization_code",
+  //         client_id: REST_API_KEY,
+  //         redirect_uri: REDIRECT_URI,
+  //         code,
+  //         client_secret: CLIENT_SECRET,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       console.log(response, "useEffect Response 출력 부분");
+  //     })
+  //     .catch((error) => {
+  //       console.log(error, "useEffect 에러 부분");
+  //     });
+  // }, []);
 
   return (
     <div>
       소셜성공
-      <br /> {code}
+      {/* <br /> {code} */}
       <br /> 페이지 입니다 🎈
     </div>
   );
