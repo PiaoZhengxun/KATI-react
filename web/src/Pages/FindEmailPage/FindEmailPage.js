2210ba9b79291f861fedc8f07cc53116a176d356
jelly-boop <70804578+jelly-boop@users.noreply.github.com>
2021-11-15 14:25:38 +0900

    find ID,PW api

--- web/src/Pages/FindEmailPage/FindEmailPage.js
+++ web/src/Pages/FindEmailPage/FindEmailPage.js
@@ -1,6 +1,43 @@
-import React from 'react'
-import { Form, FormGroup, Label, Input, Button, FormText } from 'reactstrap';
-function FindIdPage() {
+import React, { useState } from 'react';
+import { Alert, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import isEmail from 'validator/es/lib/isEmail';
+import { findEmail } from '../../api';
+function FindEmailPage() {
+
+  const [secondEmail, setSecondEmail] = useState('');
+  const [message, setMessage] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const onChange = (e) => {
+    setSecondEmail(e.target.value);
+    console.log(secondEmail);
+  };
+
+  const emailSubmit = () => {
+    //setLoading(true);
+
+    if (!isEmail(secondEmail)) {
+      setMessage('잘못된 이메일 형식 입니다.');
+    
+    } else {
+      setMessage(null);
+      findEmail
+        .findUserEmail(secondEmail)
+        .then((response) => {
+          alert(
+            ' 입력하신 ' + secondEmail + '로 이메일을 발송했습니다',
+            response,
+          );
+          window.location.href = '/login';
+         
+        })
+        .catch((error) => {
+          alert('잘못된 이메일 주소입니다', error);
+       
+        });
+    }
+  };
+
     return (
         <div style={{width: '50%', margin: '30px auto'}}>
         <div style={{display:'flex', flexDirection: "column",
@@ -12,14 +49,18 @@ function FindIdPage() {
         <Form>
         <FormGroup>
           <Label for="exampleEmail">2차 보안 이메일</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="2차 이메일을 입력하세요" />
+          <Input  onChange={onChange} type="email" name="email" id="exampleEmail" placeholder="2차 이메일을 입력하세요" />
         </FormGroup>
-
-        <Button>완료</Button>
+        <Label className={'alertArea'}>
+                {message != null ? (
+                  <Alert color="danger">{message}</Alert>
+                ) : null}
+              </Label>
+        <Button   onClick={!loading ? emailSubmit : (e) => e.preventDefault()}>완료</Button>
       </Form>
      
         </div>
     )
 }
 
-export default FindIdPage
+export default FindEmailPage
