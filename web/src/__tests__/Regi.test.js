25d6e169137c58c28b03378929b6431c3c38c379
PiaoZhengxun <pjh951218@naver.com>
2021-11-22 18:54:56 +0900

    jest test

--- web/src/__tests__/Regi.test.js
+++ web/src/__tests__/Regi.test.js
@@ -12,43 +12,4 @@ import LoginPage from "../Pages/LoginPage/LoginPage";
 
 import MockAdapter from "axios-mock-adapter";
 
-configure({ adapter: new Adapter() });
-
-describe("LoginPage Test", () => {
-  let wrapper;
-
-  test("email check", () => {
-    wrapper = shallow(<LoginPage />);
-    wrapper.find('Input[type="email"]').simulate("setEmail", {
-      target: { name: "email", value: "cksgh3422@nate.com" },
-    });
-    expect(wrapper.state("email")).toEqual("cksgh3422@nate.com");
-  });
-
-  it("password check", () => {
-    wrapper = shallow(<LoginPage />);
-    wrapper.find('Input[type="password"]').simulate("setPassword", {
-      target: {
-        name: "password",
-        value: "1234568",
-      },
-    });
-    expect(wrapper.state("password")).toEqual("1234568");
-  });
-
-  it("login check with mock email", () => {
-    wrapper = shallow(<LoginPage />);
-    wrapper.find('Input[type="email"]').simulate("setEmail", {
-      target: { name: "email", value: "cksgh3422@nate.com" },
-    });
-    wrapper.find('Input[type="password"]').simulate("setPassword", {
-      target: {
-        name: "password",
-        value: "1234568",
-      },
-    });
-    wrapper.find("button").simulate("click");
-
-    expect(wrapper.state("okLogin")).toBe(true);
-  });
-});
+// 다시
